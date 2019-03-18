49759c8bbd07016a8732b9245f34464c3a82222b
Trevor Stikeleather <Trevor@dhcp-v077-015.mobile.uci.edu>
2019-03-18 09:57:37 -0700

    made the game work kinda

--- javascript/game.js
+++ javascript/game.js
@@ -4,17 +4,14 @@ var losses = document.getElementById('losses');
 var guessesLeft = document.getElementById('guessesLeft');
 var alreadyUsed = document.getElementById('alreadyUsed');
 
+
 var letters = "abcdefghijklmnopqrstuvwxyz"
 
-// not displaying
 
-//
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 var alreadyUsedArr = []; 
-
-//shows the answer in the console
 
 //generate random letter for user to guess
 var randomLetter = letters[Math.floor(Math.random() * letters.length)];
@@ -23,59 +20,35 @@ document.onkeyup = function userInput(event) {
 //takes in user input
 var userGuess = event.key;
 
-//not working
-/*if (userGuess != letters) {
-alert('only letters of the alphabet pls')
-}
-*/
 
-/* this isn't working correctly and so the for loop is testing a different route
-if (event == randomLetter) {
-//if user guesses right then add to the wins 
-wins++;
-}
-if (guessesLeft == 0) {
-losses++;
-}
-if (event != randomLetter) {
-//if the user guesses wrong then take away from the guesses left
-guessesLeft--;
-}
-
-if (guessesLeft == 0) {
-losses++;
-}
-*/
 if (userGuess !== randomLetter && guessesLeft > 0) {
     //if the user guesses wrong then take away from the guesses left
-    guessesLeft = guessesLeft - 1;
-
+    guessesLeft--;
+}
+//got the game to reset the count of guesses but does't fully reset with a new letter/letters used
 if (guessesLeft == 0) {
 losses++;
-console.log(losses)
+guessesLeft = 10;
+
 } else if (userGuess == randomLetter) {
 //if user guesses right then add to the wins 
 wins++; 
-console.log(wins)
-} else if (userGuess !== randomLetter) {
-//if the user guesses wrong then take away from the guesses left
-guessesLeft--;
-    }
+guessesLeft = 10;
+    }       
+    //isn't flagging the letters at all
+    if (userGuess == alreadyUsedArr.length) {
+        alert('pick a letter you havent used yet');
+    } 
 
-}
 
-if (userGuess == alreadyUsedArr) {
-    alert('pick a letter you havent used yet');
-} 
 
-// alerts for every key press rather than ever non letter
-// if (userGuess !== letters) {
-    alert('that\'s not a letter silly');
-}
+
+
+
 alreadyUsedArr.push(userGuess)
-// alreadyUsedArr.push(userGuess);
-$("#guessesLeft").text('Guesses Left: ' + guessesLeft);
-/* *******is displaying two letters for some reason******* */
+
+$("#guessesLeft").text('Guesses Left: ' + guessesLeft );
+/* ******* not stopping duplicate letters from showing ******* */
 $('#alreadyUsed').text("Letters Used: " + alreadyUsedArr);
 $('#losses').text('Losses: ' + losses);
 $('#wins').text("Wins: " + wins)
@@ -85,5 +58,5 @@ console.log(alreadyUsedArr);
 
 };
 
-
+//shows the answer in the console
 console.log(randomLetter);
